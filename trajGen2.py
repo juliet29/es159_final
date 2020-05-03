c47de154fbff8a4886f1b3069f95ffffa850a424
Juliet <jnwagwuume-ezeoke@college.harvard.edu>
2020-05-03 08:52:58 -0500

    any shape

--- trajGen2.py
+++ trajGen2.py
@@ -3,13 +3,17 @@ import time
 import math
 from datetime import datetime
 import pybullet_data
-
+from path import PATH
 # connect 
+
 #physicsClient =  p.connect(p.DIRECT) # debug mode
 physicsClient =  p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) 
 planeId = p.loadURDF("plane.urdf") 
 kukaId = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0])
+#kukaId2 = p.loadURDF("kuka_iiwa/model_free_base.urdf", [0, 0, 0]) # also has 7 joints, but there are functions to exert a force on the base 
+#numJoints2 = p.getNumJoints(kukaId2)
+
 
 # rese base position and index to be 0,
 p.resetBasePositionAndOrientation(kukaId, [0, 0, 0], [0, 0, 0, 1])
@@ -19,6 +23,8 @@ kukaEndEffectorIndex = 6
 numJoints = p.getNumJoints(kukaId)
 if (numJoints != 7):
   exit()
+# print(numJoints)
+# print(numJoints2)
 
 # will eventually want to be able to move the joints -- kuka_iiwa/model_free_base.urdf
 
@@ -49,10 +55,16 @@ p.setRealTimeSimulation(useRealTimeSimulation)
 # set orientation of ee to point down
 orn = p.getQuaternionFromEuler([0, -math.pi, 0])
 
-# create a list of not time dependent positions to go through 
-poses = []
-for i in range(1,10):
-  poses.append([-0.4, 0.1*i, 0. + 0.2])
+# # create a list of not time dependent positions to go through 
+# poses = []
+# for i in range(0,30):
+#   poses.append([0.0, 0.0, 0.0+0.1*i])
+# we will now use the PATH
+poses = PATH
+# # notes on range of movement from base position 
+# # x:  [-0.7, 0.5] (forward, backwards) (negative values make robot go forward)!
+# # y: [-0.7, 0.7] (left ,right)
+# # z:  [0, 1] (down, up)
 
 # get the joint space configurations for each element of poses
 configs = []
@@ -60,35 +72,46 @@ for pos in poses:
   jointPoses = p.calculateInverseKinematics(kukaId, kukaEndEffectorIndex, pos, orn, ll, ul, jr, rp)
   configs.append(jointPoses)
 
+# reset cam
+p.resetDebugVisualizerCamera( cameraDistance=3, cameraYaw=-50, cameraPitch=-35, cameraTargetPosition=[0,0,0])
+  # need to step through time instead of having array 
 # run simulation through all configurations, and then stop 
 iter = 0
-while iter < len(configs): 
-  for i in range(len(configs)):
-    config = configs[i]
-    
-    iter +=1
-    # reset the robot to this configuration 
-    for n in range(numJoints):
-      p.resetJointState(kukaId, n, config[n])
+while 1:
+  while iter < len(configs): 
+    for i in range(len(configs)):
+      config = configs[i]
+      
+      iter +=1
+      # reset the robot to this configuration 
+      for n in range(numJoints):
+        # overrides all other physical simulations 
+        #p.resetJointState(kukaId, n, config[n])
+        #control joints to a state
+        p.setJointMotorControl2(bodyIndex = kukaId, jointIndex=n, controlMode = p.POSITION_CONTROL, targetPosition=config[n])
 
-    # show the trajectory the robot is taking
-    firstPos = poses[0]
-    currPos = poses[i]
-    black = [0,0,0]
-    debugDuration = 15
-    p.addUserDebugLine(firstPos, currPos, black, 1, debugDuration)
+      # show the trajectory the robot is taking
+      
+      black = [0,0,0]
+      debugDuration = 15
+      firstPos = poses[0]
+      currPos = poses[i]
+      if i < len(poses) -10:
+        nextPos = poses[i + 10]
+        p.addUserDebugLine(currPos, nextPos, black, 1, lifeTime=0)
 
-    # show current workspace position 
-    currPosStr = str(poses[i])
-    pauseTime = 1/5
-    p.addUserDebugText(currPosStr, [1,1,1], black, lifeTime = pauseTime)
+      # show current workspace position 
+      currPosStr = str(poses[i])
+      pauseTime = 1/100
+      p.addUserDebugText(currPosStr, [0,0,1], black, lifeTime = pauseTime)
 
-    # step the simulation 
-    p.stepSimulation()
-    
-    # rest between each step 
-    time.sleep(pauseTime)
+      # step the simulation 
+      p.stepSimulation()
+      
+      # rest between each step 
+      time.sleep(pauseTime)
+  # sleep when done then disconnect
+  time.sleep(5)
 
-
-# disconnect  
-p.disconnect()
+  # disconnect  
+  p.disconnect()
